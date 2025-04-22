6faed9e2c3d43416ba0254c8d5357d8dbe7a7410
Cory Streiff <c.streiff@pm.me>
2025-04-22 19:15:55 +0200

    Link history table with api

--- src/components/Statistics/Statistics.tsx
+++ src/components/Statistics/Statistics.tsx
@@ -11,25 +11,32 @@ const Statistics: React.FC = () => {
       <h2>Your Score</h2>
       <div className={styles.score}>{user?.score}</div>
       <h2>History</h2>
-      <div style={{ overflowX: "auto", width: "100%", padding: "0.25rem" }}>
+      <div className={styles.tableContainer}>
         <table>
           <thead>
             <tr>
               <th>Round</th>
-              <th>Score</th>
+              <th>Result</th>
               <th>Prediction</th>
               <th>Locked</th>
               <th>Final</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>1</td>
-              <td>-1</td>
-              <td>Up</td>
-              <td>$84,321.50</td>
-              <td>$84,298.30</td>
-            </tr>
+            {user?.predictions.toReversed().map((prediction, index: number) => {
+              if (!prediction.status) return;
+
+              const round = user.predictions.length - index;
+              return (
+                <tr>
+                  <td>{round}</td>
+                  <td>{prediction.status}</td>
+                  <td>{prediction.direction}</td>
+                  <td>{prediction.startPrice}</td>
+                  <td>{prediction.finalPrice}</td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
